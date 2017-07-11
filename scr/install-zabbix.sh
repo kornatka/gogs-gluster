c1edd8cc176cce806fc8ca305c0a92d0205b805d
sayo20 <osayp70@gmail.com>
2017-07-11 02:23:28 +0200

    working zabbix conf

--- scr/install-zabbix.sh
+++ scr/install-zabbix.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 sudo apt install zabbix-agent -y
 sudo service zabbix-agent stop
-sudo cp /vagrant/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
-sudo service zabbix-agent start
+if [ "$(hostname)" = "docker1" ]; then
+	sudo cp /vagrant/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
+	sudo sed -i /Hostname/s/feyi-docker/feyi-docker1/ \
+/etc/zabbix/zabbix_agentd.conf
+fi
+if [ "$(hostname)" = "docker2" ]; then
+	sudo cp /vagrant/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
+	sudo sed -i /Hostname/s/feyi-docker/feyi-docker2/ \
+/etc/zabbix/zabbix_agentd.conf
+fi
+if [ "$(hostname)" = "docker-gluster" ]; then
+	sudo cp /vagrant/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
+	sudo sed -i /Hostname/s/feyi-docker/feyi-docker3/ \
+/etc/zabbix/zabbix_agentd.conf
+fi
+sudo systemctl restart zabbix-agent
